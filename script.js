8e37238e75515bc5815a314c2f493f21f50cf9d1
James Crutchley <jamescrutchley98@gmail.com>
2023-03-10 14:56:29 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const Gameboard = () => {
+    const 
+}
+
+const CreatePlayer = (marker) => {
+    const marker = marker;
+    const move = (selection) => {
+        // update dom element and update gameboard
+    }
+    return { marker }
+}
